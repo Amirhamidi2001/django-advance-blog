117bfa54e3d89d0b42c332a8cd474c71315e4dae
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-05-22 05:39:36 +0330

    Add API documentation support using CoreAPI
    - Enabled `DEFAULT_SCHEMA_CLASS` in Django REST Framework settings
    - Added `include_docs_urls` to serve API docs at /api-docs/
    - Updated requirements.txt to include coreapi==2.3.3

--- core/core/settings.py
+++ core/core/settings.py
@@ -144,5 +144,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
